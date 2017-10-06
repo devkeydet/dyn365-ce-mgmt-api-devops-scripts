c6e99809ed0e5702e233178b6c65e02974a61f85
devkeydet <devkeydet@gmail.com>
2017-10-06 09:02:46 -0400

    Resolves #1

---
+++ RegisterAzureADApp.ps1
@@ -0,0 +1,36 @@
+# This script requires the following powershell module: https://www.powershellgallery.com/packages/AzureAD
+Param (
+	[string]$DisplayName,
+	[string]$UserName,
+	[string]$Password
+)
+
+$secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($UserName, $secpasswd)
+
+Connect-AzureAD -Credential $credential
+
+$app = Get-AzureADApplication -Filter "DisplayName eq '$($DisplayName)'"
+
+if (!$app)
+{
+    # Get the "Dynamics CRM Online" service principal
+    $svcPrincipal = Get-AzureADServicePrincipal | ? {$_.AppId -match "00000007-0000-0000-c000-000000000000"}
+
+    ### Microsoft Graph
+    $reqGraph = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
+    $reqGraph.ResourceAppId = $svcPrincipal.AppId
+
+    ## Delegated Permissions
+    #Access CRM Online as you
+    $accessCRMOnlineAsYou = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList "78ce3f0f-a1ce-49c2-8cde-64b5c0896db4","Scope"
+
+    # Apply permissions to App
+    $reqGraph.ResourceAccess = $accessCRMOnlineAsYou
+
+    $app = New-AzureADApplication -DisplayName "Foo" -PublicClient $true -RequiredResourceAccess $reqGraph
+}
+else
+{
+    Write-Output "App already existis"
+}
